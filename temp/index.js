034780525a798a233d4d2f6d92d141466d018696
Tinghui <tinghui8576cheng@gmail.com>
2022-10-10 03:36:22 +0800

    1010

---
+++ temp/index.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+class Input extends React.Component {
+    render(){
+        const [list] = useState("");
+        return(
+            <input type='text' onChange={(e) =>}
+        )
+    }
+}
+
+export default index;
