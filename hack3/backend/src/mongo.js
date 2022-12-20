98d724c6be26908bb82bc0525f5e638ef5fbf828
Tinghui <tinghui8576cheng@gmail.com>
2022-12-20 10:02:04 +0800

    submit1

---
+++ hack3/backend/src/mongo.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+import { dataInit } from "./upload.js";
+
+import "dotenv-defaults/config.js";
+
+mongoose.set("strictQuery", true);
+
+async function connect() {
+  // TODO 1 Connect to your MongoDB and call dataInit()
+  
+  if (!process.env.MONGO_URL) {
+    console.error("Missing MONGO_URL!!!");
+    process.exit(1);
+  }
+  mongoose.set("strictQuery", false);
+  mongoose
+  .connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+  })
+  .then((res) => {
+    dataInit()
+    console.log("mongo db connection created")});
+  mongoose.connection.on('error',
+      console.error.bind(console, 'connection error:'));
+  // TODO 1 End
+}
+
+export default { connect };
---
+++ hack3/frontend/src/components/Analytics.js
@@ -0,0 +1,34 @@
+import { useQuery } from '@apollo/client';
+
+import { GET_ITEMS_QUERY } from '../graphql/queries';
+
+import Balance from './Balance';
+import Category from './Category';
+
+function Analytics() {
+  // TODO 2.2 Use the useQuery hook to get items from backend
+  const {
+    loading, error, data: itemsData, subscribeToMore,
+  } = useQuery(GET_ITEMS_QUERY);
+  // TODO 2.2 End
+  const { items } = itemsData;
+
+  return (
+    <div className="grid grid-cols-12 gap-6">
+      { 
+        // TODO 2.3 Add Balence and Category (uncomment the following code)
+      }
+      <div className="col-span-6">
+        <Balance items={items} />
+      </div>
+      <div className="col-span-6">
+        <Category items={items} />
+      </div>
+      {
+        // TODO 2.3 End
+      }
+    </div>
+  );
+}
+
+export default Analytics;
